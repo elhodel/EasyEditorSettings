b0e8f358c6f01f12e6b25ee8ad421d84dbf441f0
elhodel <>
2024-05-31 23:48:22 +0200

    Add simple Style to SettingsProvider Inspector

---
+++ Editor/VisualElementExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UIElements;
+
+namespace elhodel.EasyEditorSettings
+{
+    public static class VisualElementExtensions
+    {
+        public static void SetPadding(this IStyle style, StyleLength padding)
+        {
+            style.paddingBottom = padding;
+            style.paddingLeft = padding;
+            style.paddingRight = padding;
+            style.paddingTop = padding;
+        }
+    }
+
+}
